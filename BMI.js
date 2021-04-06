0630ff4d1c4a26b900181c2affeb36ed7edc14fe
tienpham2009 <tiendx2009@gmail.com>
2021-04-06 22:23:12 +0700

    thay doi

--- BMI.js
+++ BMI.js
@@ -2,19 +2,20 @@ let cn = prompt("nhập cân nặng (kg)");
 let cc = prompt("nhập chiều cao (m)");
 BMI = cn / (cc * cc);
 if (BMI < 18.5) {
-    alert("Gầy")
+    alert("Gầy");
+} else if (18.5 <= BMI&&BMI <= 24.9) {
+    alert("Bình thường");
+} else if (25 <= BMI&&BMI <= 29.9) {
+    alert("Mập");
+} else if (30 <= BMI&&BMI<= 34.4) {
+    alert("Béo cấp 1");
+} else if (35 <= BMI&&BMI <= 39.9) {
+    alert("Béo cấp 2");
 } else {
-    if (18.5 <= BMI <= 24.9) {
-        alert("Bình thường")
-    } else if (25 <= BMI <= 29.9) {
-        alert("Mập")
-    } else if (30 <= BMI <= 34.4) {
-        alert("Béo cấp 1")
-    } else if (35 <= BMI <= 39.9) {
-        alert("Béo cấp 2")
-    } else if (BMI >= 40) {
-        alert("Nguy hiểm")
-    }
-};
+    alert("Nguy hiểm");
+}
+
+
+
 
 
